b0568650ce7c0b854698140aae8006dc75a8dc5b
311210666 <stephescsan@ciencias.unam.mx>
2019-01-10 16:23:04 -0600

    Primera fórmula de python, clase 4.

---
+++ Clases/Programas/Ejemplo1Pelota.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#Escobar Sánchez Stephanie
+print 34*3 - (1/2) * 9.81*3**2 #1/2 calcula la división entera
+print 34*3 - (1.0/2) * 9.81*3**2 #Calcula la división flotante "real"
+
+print 34*3 - (1.0/2) * 9.81*3**2
+print 34*1 - (1.0/2) * 9.81*1**2
+print 34*1.5 - (1.0/2) * 9.81*1.5**2
+print 34*5 - (1.0/2) * 9.81*5**2
+v0 = 34
+g = 9.81
+t = 5
+y = v0*t - 1.0/2*g*t**2
+print y
+
