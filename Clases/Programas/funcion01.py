719eed21c91b3d33aa3a96601699679640593572
311210666 <stephescsan@ciencias.unam.mx>
2019-01-15 14:03:53 -0600

    Problema de la clase

--- Clases/Programas/funcion01.py
+++ Clases/Programas/funcion01.py
@@ -4,6 +4,35 @@ def vAbsoluto (x):
         return (x)
     else:
         return (-x)
+
+def raiz(x):
+    h=x
+    b=1.0
+    e=0.0001
+    while vAbsoluto (b-h)>e:
+        h = (b+h)/2
+        b = x/h
+    return (b)
+
+def raiz1(x):
+    h=x
+    b=1.0
+    e=0.0001
+    while vAbsoluto(b-h)>e:
+        h = (b+h)/2
+        b = x/h
+    
+    return (b)
+    
+print raiz1 
+
+print raiz1 (1)
+print raiz1 (4)
+print raiz (9)
+print raiz (9.1)
+print raiz (1000000)
+
+
     
 
 
